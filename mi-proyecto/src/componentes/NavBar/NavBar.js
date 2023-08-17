c7bb7e82fe21986535eee95028e0894531be46d9
Julieta <bentivegna.julieta@gmail.com>
2023-08-16 21:12:34 -0300

    Actualizo mi proyecto con las consignas de la segunda pre entrega

--- mi-proyecto/src/componentes/NavBar/NavBar.js
+++ mi-proyecto/src/componentes/NavBar/NavBar.js
@@ -1,13 +1,17 @@
+import './NavBar.css'
 import CartWidget from "../CartWidget/CartWidget"
+import { NavLink, Link } from 'react-router-dom'
 
 const NavBar = () => {
     return (
-        <nav>
-            <h3>Kokiri Tienda</h3>
-            <div>
-                <button>Consolas</button>
-                <button>Videojuegos</button>
-                <button>Merchandising</button>
+        <nav className="NavBar" >
+            <Link to='/'>
+                <h3>Kokiri Tienda</h3>
+            </Link>
+            <div className='Categories'>
+                <NavLink to={`/category/consolas`} className={({ isActive}) => isActive ? 'ActiveOption' : 'Option'}>consolas</NavLink>
+                <NavLink to={`/category/videojuegos`} className={({ isActive}) => isActive ? 'ActiveOption' : 'Option'}>videojuegos</NavLink>
+                <NavLink to={`/category/merchandising`} className={({ isActive}) => isActive ? 'ActiveOption' : 'Option'}>merchandising</NavLink>
             </div>
             <CartWidget />
         </nav>
